5a0813eba90ed67a91d6bd132c145f560b99c4e3
thiago-tanaka <thiago.tanaka.dev@gmail.com>
2023-11-29 22:26:52 +0900

    add breeze + liviwire (functional)

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        port: 5177,
+        host: '0.0.0.0',
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
